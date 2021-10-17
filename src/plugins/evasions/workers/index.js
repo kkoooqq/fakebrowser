95b0121daed1dcbb26de382e1f7297c24d452b4d
kkoooqq <>
2021-10-17 09:44:42 -0400

    Remove the nasty way to handle Worker/SharedWorker/ServiceWorker.
    Since puppeteer does not support injecting Workers execution context, we use devtools protorol Runtime class to implement it.

--- src/plugins/evasions/workers/index.js
+++ src/plugins/evasions/workers/index.js
@@ -3,7 +3,6 @@
 const {PuppeteerExtraPlugin} = require('puppeteer-extra-plugin');
 
 const withUtils = require('../_utils/withUtils');
-const {kInterceptWorkerRequestTypes} = require('../../defines');
 
 class Plugin extends PuppeteerExtraPlugin {
     constructor(opts = {}) {
@@ -15,67 +14,30 @@ class Plugin extends PuppeteerExtraPlugin {
     }
 
     async onPageCreated(page) {
-        if (
-            this.opts.env.interceptWorkerRequest !== kInterceptWorkerRequestTypes.doNotIntercept
-        ) {
-            await withUtils(page).evaluateOnNewDocument(
-                (utils, env) => {
-                    if ('undefined' !== typeof ServiceWorkerContainer) {
-                        utils.replaceWithProxy(ServiceWorkerContainer.prototype, 'register', {
-                            async apply(target, thisArg, args) {
-                                console.log(`serviceWorker is registered in the browser, ${args[0]}`);
-                                const scriptUrl = utils.getCurrentScriptPath();
-                                const workerUrl = args[0];
+        await withUtils(page).evaluateOnNewDocument(
+            (utils, env) => {
+                if ('undefined' !== typeof Worker) {
+                    const _Worker = Worker;
+                    const desc = Object.getOwnPropertyDescriptor(
+                        _Worker.prototype, 'constructor',
+                    );
 
-                                if (env.interceptWorkerRequest === 1) {
-                                    // Use console.log to notify external CDPSession
-                                    console.log('$*F082/77mF092(ppt/r_tttask_postMessage', 'ServiceWorkerContainer.register', scriptUrl, workerUrl);
-
-                                    // To ensure external reception, we pause here:
-                                    await utils.sleep(1 * 1000);
-                                } else {
-                                    args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?uuid=${env.uuid}&scriptUrl=${encodeURIComponent(scriptUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
-                                }
-
-                                return Reflect.apply(target, thisArg, args);
-                            },
-                        });
-                    }
-
-                    if ('undefined' !== typeof Worker) {
-                        const _Worker = Worker;
-                        const desc = Object.getOwnPropertyDescriptor(
-                            _Worker.prototype, 'constructor',
-                        );
-
-                        utils.replaceWithProxy('undefined' === typeof window ? globalThis : window, 'Worker', {
-                            construct: async function (target, args) {
-                                // const obj = Object.create(_Worker.prototype);
+                    utils.replaceWithProxy('undefined' === typeof window ? globalThis : window, 'Worker', {
+                        construct: function (target, args) {
+                            // const obj = Object.create(_Worker.prototype);
 
                                 console.log(`worker is registered in the browser, ${args[0]}`);
                                 const scriptUrl = utils.getCurrentScriptPath();
                                 const workerUrl = args[0];
 
-                                if (env.interceptWorkerRequest === 1) {
-                                    console.log('$*F082/77mF092(ppt/r_tttask_postMessage', 'ServiceWorkerContainer.register', scriptUrl, ...args);
+                            args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?uuid=${env.uuid}&scriptUrl=${encodeURIComponent(scriptUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
 
-                                    // To ensure external reception, we pause here:
-                                    await utils.sleep(1 * 1000);
-
-                                    // Reflect.apply(target, obj, args);
-                                } else {
-                                    args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?uuid=${env.uuid}&scriptUrl=${encodeURIComponent(scriptUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
-                                }
-
-                                const obj = new desc.value(...args);
-
-                                return obj;
-                            },
-                        });
-                    }
-                }, this.opts.env,
-            );
-        }
+                            return new desc.value(...args);
+                        },
+                    });
+                }
+            }, this.opts.env,
+        );
     }
 }
 
