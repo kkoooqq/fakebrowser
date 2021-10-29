da1dc1e8ad28434b46c9e8ab4805508c6cfdcb2c
kkoooqq <>
2021-10-29 10:18:59 -0400

    bypass SharedWorker

--- src/plugins/evasions/workers/index.js
+++ src/plugins/evasions/workers/index.js
@@ -17,45 +17,85 @@ class Plugin extends PuppeteerExtraPlugin {
         await withUtils(page).evaluateOnNewDocument(
             (utils, env) => {
                 if ('undefined' !== typeof Worker) {
-                    // noinspection UnnecessaryLocalVariableJS
-                    const _Worker = Worker;
-                    const desc = Object.getOwnPropertyDescriptor(
-                        _Worker.prototype, 'constructor',
-                    );
+                    if ('undefined' !== typeof Worker) {
+                        // noinspection UnnecessaryLocalVariableJS
+                        const _Worker = Worker;
+                        const workerConstructor = Object.getOwnPropertyDescriptor(
+                            _Worker.prototype, 'constructor',
+                        );
 
-                    utils.replaceWithProxy('undefined' === typeof window ? globalThis : window, 'Worker', {
-                        construct: function (target, args) {
-                            console.log(`worker is registered in the browser, ${args[0]}`);
-                            const relUrl = window.location.href;
-                            const workerUrl = args[0];
+                        utils.replaceWithProxy('undefined' === typeof window ? globalThis : window, 'Worker', {
+                            construct: function (target, args) {
+                                console.log(`worker is registered in the browser, ${args[0]}`);
+                                const relUrl = window.location.href;
+                                const workerUrl = args[0];
 
-                            // fix: The worker's relative path is relative to the current page path.
-                            // reference: https://github.com/shehua/Alice/blob/master/w3c/html5-web-workers.md
+                                // fix: The worker's relative path is relative to the current page path.
+                                // reference: https://github.com/shehua/Alice/blob/master/w3c/html5-web-workers.md
 
-                            // noinspection LoopStatementThatDoesntLoopJS
-                            for (; ;) {
-                                if (!workerUrl) {
+                                // noinspection LoopStatementThatDoesntLoopJS
+                                for (; ;) {
+                                    if (!workerUrl) {
+                                        break;
+                                    }
+
+                                    if (
+                                        workerUrl.includes('://') &&
+                                        !(
+                                            workerUrl.startsWith('http://')
+                                            || workerUrl.startsWith('https://')
+                                        )
+                                    ) {
+                                        break;
+                                    }
+
+                                    args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?type=worker&uuid=${env.uuid}&relUrl=${encodeURIComponent(relUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
+
                                     break;
                                 }
 
-                                if (
-                                    workerUrl.includes('://') &&
-                                    !(
-                                        workerUrl.startsWith('http://')
-                                        || workerUrl.startsWith('https://')
-                                    )
-                                ) {
+                                return new workerConstructor.value(...args);
+                            },
+                        });
+                    }
+
+                    if ('undefined' !== typeof SharedWorker) {
+                        const _SharedWorker = SharedWorker;
+                        const sharedWorkerConstructor = Object.getOwnPropertyDescriptor(
+                            _SharedWorker.prototype, 'constructor',
+                        );
+
+                        utils.replaceWithProxy('undefined' === typeof window ? globalThis : window, 'SharedWorker', {
+                            construct: function (target, args) {
+                                console.log(`sharedWorker is registered in the browser, ${args[0]}`);
+                                const relUrl = window.location.href;
+                                const workerUrl = args[0];
+
+                                // noinspection LoopStatementThatDoesntLoopJS
+                                for (; ;) {
+                                    if (!workerUrl) {
+                                        break;
+                                    }
+
+                                    if (
+                                        workerUrl.includes('://') &&
+                                        !(
+                                            workerUrl.startsWith('http://')
+                                            || workerUrl.startsWith('https://')
+                                        )
+                                    ) {
+                                        break;
+                                    }
+
+                                    args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?type=sharedWorker&uuid=${env.uuid}&relUrl=${encodeURIComponent(relUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
+
                                     break;
                                 }
 
-                                args[0] = `http://127.0.0.1:7311/api/patchWorkerJsContent?uuid=${env.uuid}&relUrl=${encodeURIComponent(relUrl)}&workerUrl=${encodeURIComponent(workerUrl)}`;
-
-                                break;
-                            }
-
-                            return new desc.value(...args);
-                        },
-                    });
+                                return new sharedWorkerConstructor.value(...args);
+                            },
+                        });
+                    }
                 }
             }, this.opts.env,
         );
